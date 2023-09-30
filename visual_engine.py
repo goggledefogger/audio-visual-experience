9cdd2e3e37ce062496965d50975cf08c47eac802
townwizard <goggledefogger@users.noreply.github.com>
2023-09-29 21:59:21 -0700

    improved fractal b

--- visual_engine.py
+++ visual_engine.py
@@ -47,18 +47,31 @@ class VisualEngine:
             self.zoom *= base_zoom_factor + modulation
 
     # Class for another type of Fractal
+
     class FractalB:
         def __init__(self, screen):
             self.screen = screen
             self.reset_fractal()
 
         def reset_fractal(self):
+            self.screen.fill((0, 0, 0))  # Clear the screen
             self.vertices = [(WIDTH // 2, 50), (50, HEIGHT - 50), (WIDTH - 50, HEIGHT - 50)]
             self.current_point = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
             self.color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
             self.points_drawn = 0
+            self.directions = [(random.choice([-1, 1]), random.choice([-1, 1])) for _ in range(3)]
+            self.bg_color = (random.randint(10, 50), random.randint(10, 50), random.randint(10, 50))
 
         def draw(self):
+            # Draw a subtle gradient background
+            gradient_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+            gradient_surface.fill((*self.bg_color, 5))
+            self.screen.blit(gradient_surface, (0, 0))
+
+            # Draw the moving vertices with trails
+            for vertex, direction in zip(self.vertices, self.directions):
+                pygame.draw.circle(self.screen, (255, 255, 255, 50), vertex, 5)
+
             for _ in range(1000):  # Draw 1000 points per frame for smoother animation
                 chosen_vertex = random.choice(self.vertices)
                 new_x = (self.current_point[0] + chosen_vertex[0]) // 2
@@ -69,13 +82,24 @@ class VisualEngine:
                     (self.color[1] + new_y) % 255,
                     (self.color[2] + new_x + new_y) % 255
                 )
-                self.screen.set_at(self.current_point, gradient_color)
+                point_size = 1 + (new_x + new_y) % 3  # Vary point size between 1 and 3
+                pygame.draw.circle(self.screen, (*gradient_color, 150), self.current_point, point_size)
                 self.points_drawn += 1
-                if self.points_drawn > 50000:  # Reset after 50,000 points
+                if self.points_drawn >= 100000:  # Reset after 100,000 points
                     self.reset_fractal()
 
         def update(self):
-            # Slightly move the vertices for dynamic effect
+            # Move the vertices in a controlled manner
             for i in range(3):
-                self.vertices[i] = (self.vertices[i][0] + random.randint(-1, 1),
-                                    self.vertices[i][1] + random.randint(-1, 1))
+                if 0 < self.vertices[i][0] < WIDTH:
+                    self.vertices[i] = (self.vertices[i][0] + self.directions[i][0], self.vertices[i][1])
+                else:
+                    self.directions[i] = (-self.directions[i][0], self.directions[i][1])
+                if 0 < self.vertices[i][1] < HEIGHT:
+                    self.vertices[i] = (self.vertices[i][0], self.vertices[i][1] + self.directions[i][1])
+                else:
+                    self.directions[i] = (self.directions[i][0], -self.directions[i][1])
+            # Gradually change the base color
+            self.color = ((self.color[0] + 1) % 255, (self.color[1] + 2) % 255, (self.color[2] + 3) % 255)
+            # Gradually change the background color
+            self.bg_color = ((self.bg_color[0] + 1) % 60, (self.bg_color[1] + 1) % 60, (self.bg_color[2] + 1) % 60)
